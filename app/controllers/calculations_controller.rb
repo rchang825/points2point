a2377edf9c06114321c91e68e754e8a75f93444d
Ruby Chang <rchang825@hotmail.com>
2024-05-24 16:50:35 -0700

    Finish functional calculation page that checks credit card points against a loyalty program award amount

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,7 +4,24 @@ class CalculationsController < ApplicationController
     @loyalty_program = LoyaltyProgram.find_by_id(params['loyalty_program'])
     @goal = params['goal']
     puts "PARTNERSHIP POINTS: "
-    submitted_points = params['partnerships']&.permit!&.to_h&.entries&.map{ |a,b| {partnership_id: a, points: b['points_used']} }
+    submitted_points = params['partnerships']&.permit!&.to_h&.entries&.map{ |a,b| {card_partnership_id: a, points: b['points_used']} }
     puts submitted_points.inspect
+
+    # total_points_used = 0
+    # for each card
+    # find card_partnership with id
+    # total_points_used += points_used * card_partnership.conversion_rate
+    #
+    # points_needed = @goal - total_points_used
+    if submitted_points
+      @total_points_used = 0
+      submitted_points.each do |card_partnership|
+        partnership = CardPartnership.find_by_id(card_partnership[:card_partnership_id])
+        @total_points_used += card_partnership[:points].to_i * partnership.conversion_rate
+      end
+      @points_needed = @goal.to_i - @total_points_used
+      puts "points needed: "
+      puts @points_needed
+    end
   end
 end
